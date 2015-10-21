9f46828aecc72fa1e07c0cd6478d8f109ff38d33
Stephan Endres <stephan.endres@ecomtrading.com>
2015-10-21 09:52:03 -0600

    Hello world

---
+++ Selenium/Program.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selenium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create reference for our browser
+            IWebDriver driver = new ChromeDriver();
+            // ó IWebDriver driver = new FirefoxDriver();
+
+            //Go to google.com
+            driver.Navigate().GoToUrl("http://www.google.com");
+
+            //Get the element
+            IWebElement element = driver.FindElement(By.Id("lst-ib"));
+
+            //Perform Ops
+            element.SendKeys("Hello World");
+
+        }
+    }
+}
